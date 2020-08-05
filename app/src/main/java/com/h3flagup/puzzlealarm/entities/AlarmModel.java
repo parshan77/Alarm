fc93e6ea18646cdcf122c9120977bca502c6b7d8
parshan <parshanteimouri@yahoo.com>
2020-08-06 01:35:50 +0430

    minor changes

--- app/src/main/java/com/h3flagup/puzzlealarm/entities/AlarmModel.java
+++ app/src/main/java/com/h3flagup/puzzlealarm/entities/AlarmModel.java
@@ -1,23 +1,5 @@
 package com.h3flagup.puzzlealarm.entities;
 
 public class AlarmModel {
-    private int hour, minute;
-    private boolean[] days = {false, true, true, true, false, false, true};
 
-    public boolean[] getDays() {
-        return days;
-    }
-
-    public int getHour() {
-        return hour;
-    }
-
-    public int getMinute() {
-        return minute;
-    }
-
-    public AlarmModel(int hour, int minute) {
-        this.hour = hour;
-        this.minute = minute;
-    }
 }
